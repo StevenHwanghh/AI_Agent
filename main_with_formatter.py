ee69a1de4bcc2cd2b6a416701ba7f5584561d115
Steven Huang <steven.hwanghh@gmail.com>
2025-11-15 19:56:27 -0500

    Added RunableLambda

--- main_with_formatter.py
+++ main_with_formatter.py
@@ -5,12 +5,12 @@ load_dotenv()
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_groq import ChatGroq
-from langchain_tavily import \
-    TavilySearch  # TavilySearch 直接被包装为一个可以被LangChain使用的tool
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
+from langchain_groq import ChatGroq
+from langchain_tavily import \
+    TavilySearch  # TavilySearch 直接被包装为一个可以被LangChain使用的tool
 
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
@@ -28,15 +28,16 @@ react_prompt_with_format_instructions = PromptTemplate(
         "input",
         "agent_scratchpad",
     ],
-    partial_variables={
-        "format_instructions": output_parser.get_format_instructions()
-    },
+    partial_variables={"format_instructions": output_parser.get_format_instructions()},
 )
 
 agent = create_react_agent(llm, tools, prompt=react_prompt_with_format_instructions)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
